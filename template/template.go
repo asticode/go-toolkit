c456977fe62ab9d8ce9b366397c51562c503e522
Quentin Renard <contact@asticode.com>
2017-01-15 12:12:16 +0100

    Added template

---
+++ template/template.go
@@ -0,0 +1,43 @@
+package template
+
+import (
+	"html/template"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// ParseDirectory parses a directory recursively
+func ParseDirectory(i, ext string) (t *template.Template, err error) {
+	// Parse templates
+	t = template.New("Root")
+	return t, filepath.Walk(i, func(path string, info os.FileInfo, e error) (err error) {
+		// Check input error
+		if e != nil {
+			err = e
+			return
+		}
+
+		// Only process files
+		if info.IsDir() {
+			return
+		}
+
+		// Check extension
+		if ext != "" && filepath.Ext(path) != ext {
+			return
+		}
+
+		// Read file
+		var b []byte
+		if b, err = ioutil.ReadFile(path); err != nil {
+			return
+		}
+
+		// Add template
+		var c = t.New(strings.TrimPrefix(path, i))
+		c.Parse(string(b))
+		return
+	})
+}
---
+++ template/template_test.go
@@ -0,0 +1,17 @@
+package template_test
+
+import (
+	"testing"
+
+	"github.com/asticode/go-toolkit/template"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseDirectory(t *testing.T) {
+	tmpl, err := template.ParseDirectory("tests", "")
+	assert.NoError(t, err)
+	assert.Len(t, tmpl.Templates(), 5)
+	tmpl, err = template.ParseDirectory("tests", ".html")
+	assert.NoError(t, err)
+	assert.Len(t, tmpl.Templates(), 4)
+}
